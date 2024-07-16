d704511d4c1a91a803426bffe5c787d7f4fcee42
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-07-17 02:08:56 +0530

    Final Admin View Updated

--- gallery/core/admin.py
+++ gallery/core/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from core.models import Image
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'image', 'description', 'landscape',]
+    list_filter = ['landscape', 'date']
+    search_fields = ['name', 'description', ]
+    readonly_fields = ['date', ]
+    fieldsets = [
+        ('Details', {
+            'classes': ('wide', ),
+            'fields': ('name', 'description', 'landscape')
+        }),
+        ('Image', {
+            'fields': ('image', )
+        }),
+        ('Date', {
+            'classes': ('wide', ),
+            'fields': ('date', )
+        })
+    ]
 
-# Register your models here.
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
