23b8b7dcd88d927a86fed673e8d7ac27c9d30c10
Anurag Bhatt <72703824+anuragbhatt1805@users.noreply.github.com>
2024-08-18 19:51:09 +0530

    Update admin.py

--- gallery/core/admin.py
+++ gallery/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from core.models import Image
+from cloudinary.uploader import destroy
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['name', 'image', 'description', 'landscape',]
@@ -19,5 +20,10 @@ class ImageAdmin(admin.ModelAdmin):
             'fields': ('date', )
         })
     ]
+
+    def delete_model(self, request: HttpRequest, obj: Any) -> None:
+        if obj.image:
+            destroy(obj.image.public_id)
+        return super().delete_model(request, obj)
 
 admin.site.register(Image, ImageAdmin)
